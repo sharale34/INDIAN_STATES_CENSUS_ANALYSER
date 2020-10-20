6a60ec0c590685d660ce641d7dda6f7948c3bd06
sharale34 <sourabhharale8000@gmail.com>
2020-10-20 21:21:52 +0530

    Added: Welcome message is added

---
+++ main/java/com/bridgelabz/censusanalyser/StateCensusAnalyser.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.censusanalyser;
+
+import java.util.logging.Logger;
+
+public class StateCensusAnalyser {
+
+	public static void main(String[] args) {
+		Logger log = Logger.getLogger(StateCensusAnalyser.class.getName());
+		log.info("Welcome to the Indian States Census Analyser Problem.");
+	}
+}
