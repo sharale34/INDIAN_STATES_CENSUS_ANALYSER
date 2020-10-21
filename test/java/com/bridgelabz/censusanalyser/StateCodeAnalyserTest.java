1f8d1cb46bcac3e1258918b1107338c3a65d4605
sharale34 <sourabhharale8000@gmail.com>
2020-10-21 12:11:34 +0530

    Added: Given the state code CSV file if incorrect returns a custom exception

--- test/java/com/bridgelabz/censusanalyser/StateCodeAnalyserTest.java
+++ test/java/com/bridgelabz/censusanalyser/StateCodeAnalyserTest.java
@@ -2,9 +2,11 @@ package com.bridgelabz.censusanalyser;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class StateCodeAnalyserTest {
 	private static final String CODE_CSV_FILE_PATH = "C:\\Users\\SOURABH HARALE\\eclipse-workspace\\censusanalyser\\src\\test\\resources\\IndiaStateCode.csv";
+	private static final String WRONG_CODE_CSV_FILE_PATH = "C:\\Users\\SOURABH HARALE\\eclipse-workspace\\censusanalyser\\src\\test\\IndiaStateCode.csv";
 	@Test
 	public void givenIndianCodeCSVFile_ShouldReturnCorrectNumberOfRecordTest()  {
 		try {
@@ -12,6 +14,18 @@ public class StateCodeAnalyserTest {
 			int numOfRecords = censusAnalyser.readCodeData(CODE_CSV_FILE_PATH);
 			Assert.assertEquals(29, numOfRecords);
 		} catch (CensusAnalyserException e) {
+		}
+	}
+	
+	@Test
+	public void givenWrongIndianCodeCSVFile_ShouldReturnCustomExceptionTest() {
+		try {
+			StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
+			ExpectedException exceptionRule = ExpectedException.none();
+			exceptionRule.expect(CensusAnalyserException.class);
+			censusAnalyser.readCensusData(WRONG_CODE_CSV_FILE_PATH);
+		} catch (CensusAnalyserException e) {
+			Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_EXCEPTION, e.type);
 		}
 	}
 }
