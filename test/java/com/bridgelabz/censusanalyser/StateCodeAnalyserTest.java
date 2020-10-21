a08ac25de995d96f4c41bb9590448fd874cd287d
sharale34 <sourabhharale8000@gmail.com>
2020-10-21 12:17:49 +0530

    Added: Given the state code CSV file when correct but type incorrect return a custom exception

--- test/java/com/bridgelabz/censusanalyser/StateCodeAnalyserTest.java
+++ test/java/com/bridgelabz/censusanalyser/StateCodeAnalyserTest.java
@@ -7,8 +7,10 @@ import org.junit.rules.ExpectedException;
 public class StateCodeAnalyserTest {
 	private static final String CODE_CSV_FILE_PATH = "C:\\Users\\SOURABH HARALE\\eclipse-workspace\\censusanalyser\\src\\test\\resources\\IndiaStateCode.csv";
 	private static final String WRONG_CODE_CSV_FILE_PATH = "C:\\Users\\SOURABH HARALE\\eclipse-workspace\\censusanalyser\\src\\test\\IndiaStateCode.csv";
+	private static final String WRONG_CODE_CSV_FILE_TYPE = "C:\\Users\\SOURABH HARALE\\eclipse-workspace\\censusanalyser\\src\\test\\IndiaStateCode.java";
+
 	@Test
-	public void givenIndianCodeCSVFile_ShouldReturnCorrectNumberOfRecordTest()  {
+	public void givenIndianCodeCSVFile_ShouldReturnCorrectNumberOfRecordTest() {
 		try {
 			StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
 			int numOfRecords = censusAnalyser.readCodeData(CODE_CSV_FILE_PATH);
@@ -16,7 +18,7 @@ public class StateCodeAnalyserTest {
 		} catch (CensusAnalyserException e) {
 		}
 	}
-	
+
 	@Test
 	public void givenWrongIndianCodeCSVFile_ShouldReturnCustomExceptionTest() {
 		try {
@@ -26,6 +28,18 @@ public class StateCodeAnalyserTest {
 			censusAnalyser.readCensusData(WRONG_CODE_CSV_FILE_PATH);
 		} catch (CensusAnalyserException e) {
 			Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_EXCEPTION, e.type);
+		}
+	}
+
+	@Test
+	public void givenWrongTypeIndianCodeCSVFile_ShouldReturnCustomExceptionTest() {
+		try {
+			StateCensusAnalyser codeAnalyser = new StateCensusAnalyser();
+			ExpectedException exceptionRule = ExpectedException.none();
+			exceptionRule.expect(CensusAnalyserException.class);
+			codeAnalyser.readCodeData(WRONG_CODE_CSV_FILE_TYPE);
+		} catch (CensusAnalyserException e) {
+			Assert.assertEquals(CensusAnalyserException.ExceptionType.CODE_FILE_EXCEPTION, e.type);
 		}
 	}
 }
