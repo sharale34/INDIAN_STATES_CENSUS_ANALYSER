e910544550e8af7a072fd672293ab24777e23eb5
sharale34 <sourabhharale8000@gmail.com>
2020-10-21 10:04:54 +0530

    Added: Given the state census CSV file, if incorrect returns a custom exception

--- main/java/com/bridgelabz/censusanalyser/StateCensusAnalyser.java
+++ main/java/com/bridgelabz/censusanalyser/StateCensusAnalyser.java
@@ -18,10 +18,13 @@ public class StateCensusAnalyser {
 
 	public int readCensusData(String csvFilePath) throws CensusAnalyserException {
 		try {
+			//Abstract class for reading character streams
 			Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
+			//This class makes it possible to bypass all the intermediate steps and classes in setting up to read from a CSV source to a list of beans. 
 			CsvToBeanBuilder<CSVStateCensus> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
 			csvToBeanBuilder.withType(CSVStateCensus.class);
 			csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+			//Converts CSV data to objects
 			CsvToBean<CSVStateCensus> csvToBean = csvToBeanBuilder.build();
 			Iterator<CSVStateCensus> censusCSVIterator = csvToBean.iterator();
 			int numOfEntries = 0;
--- test/java/com/bridgelabz/censusanalyser/StateCensusAnalyserTest.java
+++ test/java/com/bridgelabz/censusanalyser/StateCensusAnalyserTest.java
@@ -6,14 +6,28 @@ import org.junit.rules.ExpectedException;
 
 public class StateCensusAnalyserTest {
 	private static String INDIA_CENSUS_CSV_FILE_PATH = "C:\\Users\\SOURABH HARALE\\eclipse-workspace\\censusanalyser\\src\\test\\resources\\StateCensus.csv";
+	private static String WRONG_CSV_FILE_PATH = "C:\\Users\\SOURABH HARALE\\eclipse-workspace\\censusanalyser\\StateCensus.csv";
 
 	@Test
-	public void givenIndianCensusCSVFile_ShouldReturnCorrectNumberOfRecordTest() {
+	public void givenIndianCensusCSVFile_ShouldReturnCorrectNumberOfRecordTest(){
 		try {
 			StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
 			int numOfRecords = censusAnalyser.readCensusData(INDIA_CENSUS_CSV_FILE_PATH);
 			Assert.assertEquals(29, numOfRecords);
 		} catch (CensusAnalyserException e) {
+		}
+	}
+
+	@Test
+	public void givenWrongIndianCensusCSVFile_ShouldReturnCustomExceptionTest() {
+		try {
+			StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
+			// Allow us to verify the Exception thrown
+			ExpectedException exceptionRule = ExpectedException.none();
+			exceptionRule.expect(CensusAnalyserException.class);
+			censusAnalyser.readCensusData(WRONG_CSV_FILE_PATH);
+		} catch (CensusAnalyserException e) {
+			Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_EXCEPTION, e.type);
 		}
 	}
 }
